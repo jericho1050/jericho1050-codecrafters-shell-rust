92667755c8f3607abed74cb2e98cb15b0470b031
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-02-01 09:36:47 +0800

    the echo builtin

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
+use regex::Regex;
 #[allow(unused_imports)]
 use std::io::{self, Write};
-
+use std::{os::unix::process, process::exit};
 fn main() {
     // Uncomment this block to pass the first stage
-    
-    while true {
-    print!("$ ");
-    io::stdout().flush().unwrap();
 
-    // Wait for user input
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim())  
+        let parts: Vec<&str> = input.as_str().split(" ").collect();
+        let (command, args) = (parts[0], &parts[1..]);
+
+        match command {
+            "exit 0" => exit(0),
+            command => {
+                if command.starts_with("echo") {
+                    print!("{}", args.join(" "));
+                } else {
+                    println!("{}: command not found", input.trim())
+                }
+            }
+        }
     }
-    
 }
-  
