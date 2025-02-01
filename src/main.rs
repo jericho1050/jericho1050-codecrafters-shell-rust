df76750f263e6aae1c412fd20292bb69023c8a12
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-02-01 19:00:03 +0800

    The type builtin: executable files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use regex::Regex;
+use std::env::split_paths;
 #[allow(unused_imports)]
+use std::fs::metadata;
 use std::io::{self, Write};
-use std::{os::unix::process, process::exit};
-fn main() {
-    // Uncomment this block to pass the first stage
+use std::path::Path;
+use std::{env, os::unix::process, process::exit};
 
+fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -14,6 +15,7 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
         let parts: Vec<&str> = input.as_str().split_whitespace().collect();
         let (command, args) = (parts[0], &parts[1..]);
+        let path = env::var("PATH").unwrap_or_default();
 
         match command {
             "exit" => exit(0),
@@ -23,9 +25,27 @@ fn main() {
             "type" => {
                 if !args.is_empty() {
                     let sub_command = args[0].trim();
-                    match sub_command {
-                        "exit" | "echo" | "type" => println!("{} is a shell builtin", sub_command),
-                        _ => println!("{}: not found", sub_command),
+                    let directories = split_paths(&path);
+                    let mut found = false;
+                    for dir in directories {
+                        let new_path = dir.join(sub_command);
+                        let metadata = match metadata(&new_path) {
+                            Ok(m) => m,
+                            Err(_) => continue,
+                        };
+                        if new_path.exists() && metadata.is_file() {
+                            println!("{} is {}", sub_command, new_path.display());
+                            found = true;
+                            break;
+                        }
+                    }
+                    if !found {
+                        match sub_command {
+                            "exit" | "echo" | "type" => {
+                                println!("{} is a shell builtin", sub_command)
+                            }
+                            _ => println!("{}: not found", sub_command),
+                        }
                     }
                 }
             }
