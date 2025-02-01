65970a4f7e67750d5837c481dee6c7ca26876052
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-02-01 17:36:36 +0800

    the type builtin: builtins

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,28 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        let parts: Vec<&str> = input.as_str().split(" ").collect();
+        let parts: Vec<&str> = input.as_str().split_whitespace().collect();
         let (command, args) = (parts[0], &parts[1..]);
 
-        match input.trim() {
-            "exit 0" => exit(0),
-            command => {
-                if command.starts_with("echo") {
-                    print!("{}", args.join(" "));
+        match command {
+            "exit" => exit(0),
+            "echo" => {
+                println!("{}", args.join(" "));
+            }
+            "type" => {
+                if !args.is_empty() {
+                    let sub_command = args[0].trim();
+                    match sub_command {
+                        "exit" | "echo" | "type" => println!("{} is a shell builtin", sub_command),
+                        _ => println!("{}: not found", sub_command),
+                    }
+                }
+            }
+            _ => {
+                if input.trim().is_empty() {
+                    println!();
                 } else {
-                    println!("{}: command not found", input.trim())
+                    println!("{}: command not found", input.trim());
                 }
             }
         }
